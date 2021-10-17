d665af46b9f40b0f5975f1c5ac1e42db3a15d1dd
HarshRepo <amarmalvi999@gmail.com>
2021-10-17 21:14:07 +0530

    17th oct

--- router/index.js
+++ router/index.js
@@ -3,6 +3,12 @@ const router = require('express').Router();
 router.get('/',(req, res) =>{
     res.render('index');
 });
+router.get('/contact',(req, res) =>{
+    res.render('contact');
+});
+router.get('/about',(req, res) =>{
+    res.render('about');
+});
 
 router.all('*', (req,res) =>{
     res.status(404).render('error404')
